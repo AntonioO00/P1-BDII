60773bb90f1f3214775120437ccaec6a01726531
victor <vitormachado2001@gmail.com>
2024-06-17 14:50:29 -0300

    Drop tables

---
+++ SQL/DropTable.sql
@@ -0,0 +1,10 @@
+drop table transportadora cascade constraints;
+drop table produto cascade constraints;
+drop table possui cascade constraints;
+drop table pedido cascade constraints;
+drop table item cascade constraints;
+drop table cliente cascade constraints;
+drop table categoria cascade constraints;
+drop table avaliacao cascade constraints;
+drop table endereco cascade constraints;
+
